94a5074a3be14b50aa846fcb94f974f42c65ec2b
Romain G <gasq.rom@gmail.com>
2019-06-06 16:57:04 +0200

    TASK-2: Add test & linter 	* Add an entrypoint into docker file for running eslint/start/test command of yarn 	* Add unit test for general rules 	* Add unit test for dafalgan feature
    Signed-off-by: Romain G <gasq.rom@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,15 @@ FROM node:10.11
 
 WORKDIR /usr/src
 
-COPY package.json /usr/src/package.json
+COPY ./package.json /usr/src/
+COPY ./pipeline_inato.sh /usr/src/
+COPY ./yarn.lock /usr/src/
+COPY ./.babelrc /usr/src/
+COPY ./.eslintrc.yml /usr/src/
+COPY ./index.js /usr/src/
+COPY ./pharmacy.js /usr/src/
+COPY ./pharmacy.test.js /usr/src/
+
 RUN yarn install
 
-COPY . /usr/src/
-
-CMD ["yarn", "start"]
+ENTRYPOINT ["/bin/sh", "/usr/src/pipeline_inato.sh"]
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,59 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
-describe("Pharmacy", () => {
+describe("Pharmacy unit test", () => {
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
     );
   });
+});
+
+describe("Pharmacy Drug rules", () => {
+   it("Generic should decrease of 1 in benefit & expire in 2 days", () => {
+       expect(new Pharmacy([new Drug("Generic", 3, 2)]).updateBenefitValue())
+           .toEqual([new Drug("Generic", 2, 1)]);
+   }); 
+   it("Generic should decrease of 2 in benefit & expire since 1 day", () => {
+        expect(new Pharmacy([new Drug("Generic", 0, 2)]).updateBenefitValue())
+            .toEqual([new Drug("Generic", -1, 0)]);
+   });
+   it("Magic Pill should not decrease in benefit & not expire", () => {
+       expect(new Pharmacy([new Drug("Magic Pill", 30, 23)]).updateBenefitValue())
+           .toEqual([new Drug("Magic Pill", 30, 23)]);
+   }); 
+   it("Herbal Thea should increase of 1 in benefit & expire in 1 day", () => {
+        expect(new Pharmacy([new Drug("Herbal Tea", 2, 0)]).updateBenefitValue())
+            .toEqual([new Drug("Herbal Tea", 1, 1)]);
+   });
+    
+   it("Herbal Thea should increase of 2 in benefit & expire since 1 day", () => {
+        expect(new Pharmacy([new Drug("Herbal Tea", 0, 0)]).updateBenefitValue())
+            .toEqual([new Drug("Herbal Tea", -1, 2)]);
+   });
+    
+   it("Fervex should increase of 2 in benefit & expire in 9 day", () => {
+        expect(new Pharmacy([new Drug("Fervex", 10, 0)]).updateBenefitValue())
+            .toEqual([new Drug("Fervex", 9, 2)]);
+   });
+   
+   it("Fervex should increase of 3 in benefit & expire in 4 day", () => {
+        expect(new Pharmacy([new Drug("Fervex", 5, 0)]).updateBenefitValue())
+            .toEqual([new Drug("Fervex", 4, 3)]);
+   });
+    
+   it("Fervex should drop to 0 in benefit & expire since 1 day", () => {
+        expect(new Pharmacy([new Drug("Fervex", 0, 3)]).updateBenefitValue())
+            .toEqual([new Drug("Fervex", -1, 0)]);
+   });
+});
+
+describe("Pharmacy new feature : Dafalgan", () => {
+   it("Generic should decrease of 2 in benefit & expire in 2 days", () => {
+       expect(new Pharmacy([new Drug("Dafalgan", 3, 2)]).updateBenefitValue())
+           .toEqual([new Drug("Dafalgan", 2, 0)]);
+   });
+   it("Generic should decrease of 4 in benefit & expire since 1 day", () => {
+       expect(new Pharmacy([new Drug("Dafalgan", 0, 4)]).updateBenefitValue())
+           .toEqual([new Drug("Dafalgan", -1, 0)]);
+   });
 });
